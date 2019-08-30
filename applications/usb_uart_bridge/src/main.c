fbbf94fe54b01c955161fa0e2bd9b37445b4d92b
Bernt Johan Damslora <bernt.johan.damslora@nordicsemi.no>
2019-08-30 15:16:15 +0200

    applications: usb_uart_bridge: Adjust buffer size
    The size of the dynamically allocated buffers is adjusted to line up
    with the heap blocks. This decreases wasted heap space.
    
    Signed-off-by: Bernt Johan Damslora <bernt.johan.damslora@nordicsemi.no>

--- applications/usb_uart_bridge/src/main.c
+++ applications/usb_uart_bridge/src/main.c
@@ -26,7 +26,16 @@ u8_t *usb_update_sn_string_descriptor(void)
 #define POWER_THREAD_STACKSIZE		1024
 #define POWER_THREAD_PRIORITY		7
 
-#define UART_BUF_SIZE 58
+/* Heap block space is always one of 2^(2n) for n from 3 to 7.
+ * (see reference/kernel/memory/heap.html for more info)
+ * Here, we target 64-byte blocks. Since we want to fit one struct uart_data
+ * into each block, the block layout becomes:
+ * 16 bytes: reserved by the Zephyr heap block descriptor (not in struct)
+ *  4 bytes: reserved by the Zephyr FIFO (in struct)
+ * 40 bytes: UART data buffer (in struct)
+ *  4 bytes: length field (in struct, padded for alignment)
+ */
+#define UART_BUF_SIZE 40
 
 static K_FIFO_DEFINE(usb_0_tx_fifo);
 static K_FIFO_DEFINE(usb_1_tx_fifo);
