6acb821240881f81c79472f50e93b778be51f05f
Bernt Johan Damslora <bernt.johan.damslora@nordicsemi.no>
2019-12-06 14:47:11 +0100

    add led blink

--- samples/nrf9160/gps_test/src/main.c
+++ samples/nrf9160/gps_test/src/main.c
@@ -9,6 +9,8 @@
 #include <net/socket.h>
 #include <stdio.h>
 
+#include <drivers/gpio.h> // LED
+
 #define AT_XSYSTEMMODE	"AT\%XSYSTEMMODE=0,0,1,0"
 #define AT_CFUN		"AT+CFUN=1"
 
@@ -371,18 +373,29 @@ int main(void)
 	printk("Getting GPS data...\n");
 	printk("imei,seq,timestamp,longitude,latitude,altitude,accuracy,speed,heading,date,time,pdop,hdop,vdop,tdop,flags,sv0_sv,sv0_cn0,sv0_elevation,sv0_azimuth,sv0_flags,sv0_signal,sv1_sv,sv1_cn0,sv1_elevation,sv1_azimuth,sv1_flags,sv1_signal,sv2_sv,sv2_cn0,sv2_elevation,sv2_azimuth,sv2_flags,sv2_signal,sv3_sv,sv3_cn0,sv3_elevation,sv3_azimuth,sv3_flags,sv3_signal,sv4_sv,sv4_cn0,sv4_elevation,sv4_azimuth,sv4_flags,sv4_signal,sv5_sv,sv5_cn0,sv5_elevation,sv5_azimuth,sv5_flags,sv5_signal,sv6_sv,sv6_cn0,sv6_elevation,sv6_azimuth,sv6_flags,sv6_signal,sv7_sv,sv7_cn0,sv7_elevation,sv7_azimuth,sv7_flags,sv7_signal,sv8_sv,sv8_cn0,sv8_elevation,sv8_azimuth,sv8_flags,sv8_signal,sv9_sv,sv9_cn0,sv9_elevation,sv9_azimuth,sv9_flags,sv9_signal,sv10_sv,sv10_cn0,sv10_elevation,sv10_azimuth,sv10_flags,sv10_signal,sv11_sv,sv11_cn0,sv11_elevation,sv11_azimuth,sv11_flags,sv11_signal\n");
 
+	// Init LED
+	u32_t cnt0, cnt1, cnt2;
+	struct device *dev;
+	dev = device_get_binding(DT_ALIAS_LED0_GPIOS_CONTROLLER);
+	/* Set LED pin as output */
+	gpio_pin_configure(dev, DT_ALIAS_LED0_GPIOS_PIN, GPIO_DIR_OUT);
+	gpio_pin_configure(dev, DT_ALIAS_LED1_GPIOS_PIN, GPIO_DIR_OUT);
+	gpio_pin_configure(dev, DT_ALIAS_LED2_GPIOS_PIN, GPIO_DIR_OUT);
+
 	while (1) {
 
 		process_gps_data(&gps_data);
-
+		
 		if (new_pvt_data) {
 			new_pvt_data = false;
+			gpio_pin_write(dev, DT_ALIAS_LED0_GPIOS_PIN, cnt0++ % 2);
 			print_pvt_csv(&gps_data);
 			//print_pvt_data(&gps_data);
 		}
 
 		if (got_first_fix && !printed_fix_time) {
 			printed_fix_time = true;
+			gpio_pin_write(dev, DT_ALIAS_LED1_GPIOS_PIN, 1);
 			printk("***** First fix time: %d\n",
 				k_uptime_get_32() - gps_started);
 		}
