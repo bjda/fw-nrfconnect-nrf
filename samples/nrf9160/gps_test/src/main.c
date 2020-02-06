13c660772fe9cd0b66bffcfa4f0c1079f70861dd
Bernt Johan Damslora <bernt.johan.damslora@nordicsemi.no>
2020-02-06 16:33:43 +0100

    add button reset

--- samples/nrf9160/gps_test/src/main.c
+++ samples/nrf9160/gps_test/src/main.c
@@ -8,6 +8,7 @@
 #include <nrf_socket.h>
 #include <net/socket.h>
 #include <stdio.h>
+#include <misc/reboot.h>
 
 #include <drivers/gpio.h> // LED
 
@@ -365,15 +366,6 @@ int main(void)
 
 	printk("Staring GPS application\n");
 
-	if (init_app() != 0) {
-		return -1;
-	}
-
-	gps_started = k_uptime_get_32();
-
-	printk("Getting GPS data...\n");
-	printk("imei,seq,timestamp,longitude,latitude,altitude,accuracy,speed,heading,date,time,pdop,hdop,vdop,tdop,flags,sv0_sv,sv0_cn0,sv0_elevation,sv0_azimuth,sv0_flags,sv0_signal,sv1_sv,sv1_cn0,sv1_elevation,sv1_azimuth,sv1_flags,sv1_signal,sv2_sv,sv2_cn0,sv2_elevation,sv2_azimuth,sv2_flags,sv2_signal,sv3_sv,sv3_cn0,sv3_elevation,sv3_azimuth,sv3_flags,sv3_signal,sv4_sv,sv4_cn0,sv4_elevation,sv4_azimuth,sv4_flags,sv4_signal,sv5_sv,sv5_cn0,sv5_elevation,sv5_azimuth,sv5_flags,sv5_signal,sv6_sv,sv6_cn0,sv6_elevation,sv6_azimuth,sv6_flags,sv6_signal,sv7_sv,sv7_cn0,sv7_elevation,sv7_azimuth,sv7_flags,sv7_signal,sv8_sv,sv8_cn0,sv8_elevation,sv8_azimuth,sv8_flags,sv8_signal,sv9_sv,sv9_cn0,sv9_elevation,sv9_azimuth,sv9_flags,sv9_signal,sv10_sv,sv10_cn0,sv10_elevation,sv10_azimuth,sv10_flags,sv10_signal,sv11_sv,sv11_cn0,sv11_elevation,sv11_azimuth,sv11_flags,sv11_signal\n");
-
 	// Init LED
 	u32_t cnt0, cnt1, cnt2;
 	struct device *dev;
@@ -383,8 +375,27 @@ int main(void)
 	gpio_pin_configure(dev, DT_ALIAS_LED1_GPIOS_PIN, GPIO_DIR_OUT);
 	gpio_pin_configure(dev, DT_ALIAS_LED2_GPIOS_PIN, GPIO_DIR_OUT);
 
-	while (1) {
+	// Init button, cheat and assume it is same device as led
+	gpio_pin_configure(dev, DT_ALIAS_SW0_GPIO_PIN, GPIO_DIR_IN | GPIO_PUD_PULL_UP);
+	int btn_state = 0;
+	while (btn_state == 0){
+		gpio_pin_read(dev,DT_ALIAS_SW0_GPIO_PIN,&btn_state);
+	}
 
+	if (init_app() != 0) {
+		return -1;
+	}
+
+	gps_started = k_uptime_get_32();
+
+	printk("Getting GPS data...\n");
+	printk("imei,seq,timestamp,longitude,latitude,altitude,accuracy,speed,heading,date,time,pdop,hdop,vdop,tdop,flags,sv0_sv,sv0_cn0,sv0_elevation,sv0_azimuth,sv0_flags,sv0_signal,sv1_sv,sv1_cn0,sv1_elevation,sv1_azimuth,sv1_flags,sv1_signal,sv2_sv,sv2_cn0,sv2_elevation,sv2_azimuth,sv2_flags,sv2_signal,sv3_sv,sv3_cn0,sv3_elevation,sv3_azimuth,sv3_flags,sv3_signal,sv4_sv,sv4_cn0,sv4_elevation,sv4_azimuth,sv4_flags,sv4_signal,sv5_sv,sv5_cn0,sv5_elevation,sv5_azimuth,sv5_flags,sv5_signal,sv6_sv,sv6_cn0,sv6_elevation,sv6_azimuth,sv6_flags,sv6_signal,sv7_sv,sv7_cn0,sv7_elevation,sv7_azimuth,sv7_flags,sv7_signal,sv8_sv,sv8_cn0,sv8_elevation,sv8_azimuth,sv8_flags,sv8_signal,sv9_sv,sv9_cn0,sv9_elevation,sv9_azimuth,sv9_flags,sv9_signal,sv10_sv,sv10_cn0,sv10_elevation,sv10_azimuth,sv10_flags,sv10_signal,sv11_sv,sv11_cn0,sv11_elevation,sv11_azimuth,sv11_flags,sv11_signal\n");
+
+	while (1) {
+		gpio_pin_read(dev,DT_ALIAS_SW0_GPIO_PIN,&btn_state);
+		if (btn_state == 0) {
+			NVIC_SystemReset();
+		}
 		do {
 			/* Loop until we don't have more
 			 * data to read
